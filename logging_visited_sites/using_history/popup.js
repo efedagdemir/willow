ae335c7052b17b33c04ce1e5e193b7cd02f9013d
Can Cebeci <can.cebeci99@gmail.com>
2021-01-07 13:52:16 +0300

    removed cem's directory

--- logging_visited_sites/using_history/popup.js
+++
@@ -1,6 +0,0 @@
-chrome.storage.local.get("visitedPages", function(result) {
-    result.visitedPages.forEach(element => {
-        document.write(element);
-        document.write("<br>");
-    });
-});
