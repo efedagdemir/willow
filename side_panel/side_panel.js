effc6cae1c29ed4426aa8f180534873153826462
Can Cebeci <can.cebeci99@gmail.com>
2021-01-07 11:56:03 +0300

    iframe grows/shrinks dynamially using message passing

--- side_panel/side_panel.js
+++ side_panel/side_panel.js
@@ -1,11 +1,24 @@
+let panelWidth = "400px";   // this has to be the same as the definition in side_panel_ContentScript.js.
+                            // find a way to share these constants.
+
 /* Set the width of the sidebar to 250px (show it) */
 function openNav() {
-document.getElementById("mySidepanel").style.width = "400px";
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {command: "open"}, function(response) {
+            console.log(response.farewell);
+        });
+    });
+    document.getElementById("mySidepanel").style.width = panelWidth;
 }
 
 /* Set the width of the sidebar to 0 (hide it) */
 function closeNav() {
-document.getElementById("mySidepanel").style.width = "0px";
+    document.getElementById("mySidepanel").style.width = "0px";
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {command: "close"}, function(response) {
+            console.log(response.farewell);
+        });
+    }); 
 } 
 
 document.getElementById("openbtnInst").onclick = openNav;
--- side_panel/side_panel_ContentScript.js
+++ side_panel/side_panel_ContentScript.js
@@ -1,16 +1,33 @@
+let panelWidth = "400px";
+let iconWidth = "60x";
+let iconHeight = "70px";
+
 var openBtn = document.createElement('button');
 openBtn.style.position = 'fixed';
 
 // The part below is borrowed form Tuana's code
 var iframe  = document.createElement ('iframe');
 iframe.src  = chrome.runtime.getURL('side_panel.html');
-iframe.setAttribute('id', 'myframe');
 iframe.style.position = 'fixed';
 iframe.style.top='0%'
 iframe.style.left='0%'
-iframe.style.height = "100%"
-iframe.style.width = "400px";
+// initally, the iframe is icon-sized, not panel-sized
+iframe.style.width = iconWidth;
+iframe.style.height = iconHeight;
+
 document.body.zIndex = -1;
 iframe.style.zIndex = 0x7FFFFFFF;
-//iframe.style.backgroundColor = "#A0A0A0";
+
 document.body.append(iframe);
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.command == "open") {
+            iframe.style.height = "100%"
+            iframe.style.width = panelWidth;
+        } else if (request.command == "close") {
+            iframe.style.width = iconWidth;
+            iframe.style.height = iconHeight;
+        }
+    }
+  );
