33ace3e9cf5237e0e8f440a835edab082ddaaaed
efedagdemir <efedagdemir@gmail.com>
2021-01-08 01:19:22 +0300

    dagre layout

--- demo/popup.js
+++ demo/popup.js
@@ -67,9 +67,32 @@ function initGraph(nodes,edges){
             edges: edges
         },
         layout: {
-            name: 'concentric',
+            name: 'dagre',
 
-            fit: true, // whether to fit the viewport to the graph
+            // dagre algo options, uses default value on undefined
+            nodeSep: undefined, // the separation between adjacent nodes in the same rank
+            edgeSep: undefined, // the separation between adjacent edges in the same rank
+            rankSep: undefined, // the separation between each rank in the layout
+            rankDir: undefined, // 'TB' for top to bottom flow, 'LR' for left to right,
+            ranker: undefined, // Type of algorithm to assign a rank to each node in the input graph. Possible values: 'network-simplex', 'tight-tree' or 'longest-path'
+            minLen: function( edge ){ return 1; }, // number of ranks to keep between the source and target of the edge
+            edgeWeight: function( edge ){ return 1; }, // higher weight edges are generally made shorter and straighter than lower weight edges
+
+            // general layout options
+            fit: true, // whether to fit to viewport
+            padding: 30, // fit padding
+            spacingFactor: undefined, // Applies a multiplicative factor (>0) to expand or compress the overall area that the nodes take up
+            nodeDimensionsIncludeLabels: false, // whether labels should be included in determining the space used by a node
+            animate: false, // whether to transition the node positions
+            animateFilter: function( node, i ){ return true; }, // whether to animate specific nodes when animation is on; non-animated nodes immediately go to their final positions
+            animationDuration: 500, // duration of animation in ms if enabled
+            animationEasing: undefined, // easing of animation if enabled
+            boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+            transform: function( node, pos ){ return pos; }, // a function that applies a transform to the final node position
+            ready: function(){}, // on layoutready
+            stop: function(){} // on layoutstop
+
+            /*fit: true, // whether to fit the viewport to the graph
             padding: 30, // the padding on fit
             startAngle: 3 / 2 * Math.PI, // where nodes start in radians
             sweep: undefined, // how many radians should be between the first and last node (defaults to full circle)
@@ -95,6 +118,7 @@ function initGraph(nodes,edges){
             ready: undefined, // callback on layoutready
             stop: undefined, // callback on layoutstop
             transform: function (node, position ){ return position; } // transform a given node position. Useful for changing flow direction in discrete layouts
+            */
         },
           // so we can see the ids
         style: [
@@ -103,7 +127,18 @@ function initGraph(nodes,edges){
               style: {
                 'label': 'data(name)'
               }
-            }
+            },
+
+            {
+                selector: 'edge',
+                style: {
+                  'width': 3,
+                  'line-color': '#ccc',
+                  'target-arrow-color': '#ccc',
+                  'target-arrow-shape': 'triangle',
+                  'curve-style': 'bezier'
+                }
+              }
           ]
       });
       console.log(cy);
