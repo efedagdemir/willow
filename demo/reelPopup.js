fe9e597b42eeb89ca9ccd9edf9052dfdadee5649
Cem <Cebeci>
2021-02-03 01:39:28 +0300

    Rewrote the backend, should be stable now.
    The old version is still commented below until the next commit. Just in case.

---
+++ demo/reelPopup.js
@@ -0,0 +1,17 @@
+let textField = document.getElementById("text");
+textField.innerHTML = "";
+
+console.log(chrome.extension.getBackgroundPage().getSessionGraph().entries());
+
+chrome.extension.getBackgroundPage().getSessionGraph().forEach( (value) => {
+    console.log("hey");
+    treePrint(value, "");
+});
+
+function treePrint(node, prefix) {
+    textField.innerHTML += "<br>" + node.openTabCount + prefix + node.title;
+
+    node.children.forEach( child => {
+        treePrint(child, prefix + ".....");    
+    });
+}
