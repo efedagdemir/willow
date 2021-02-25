ccaa19170917e3c6423cb064cd34aa8beb233ccb
efedagdemir <efedagdemir@gmail.com>
2021-02-25 15:59:31 +0300

    minor visual change

--- demo/popup.js
+++ demo/popup.js
@@ -93,34 +93,6 @@ function initGraph(nodes,edges){
             transform: function( node, pos ){ return pos; }, // a function that applies a transform to the final node position
             ready: function(){}, // on layoutready
             stop: function(){} // on layoutstop
-
-            /*fit: true, // whether to fit the viewport to the graph
-            padding: 30, // the padding on fit
-            startAngle: 3 / 2 * Math.PI, // where nodes start in radians
-            sweep: undefined, // how many radians should be between the first and last node (defaults to full circle)
-            clockwise: true, // whether the layout should go clockwise (true) or counterclockwise/anticlockwise (false)
-            equidistant: false, // whether levels have an equal radial distance betwen them, may cause bounding box overflow
-            minNodeSpacing: 10, // min spacing between outside of nodes (used for radius adjustment)
-            boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
-            avoidOverlap: true, // prevents node overlap, may overflow boundingBox if not enough space
-            nodeDimensionsIncludeLabels: false, // Excludes the label when calculating node bounding boxes for the layout algorithm
-            height: undefined, // height of layout area (overrides container height)
-            width: undefined, // width of layout area (overrides container width)
-            spacingFactor: undefined, // Applies a multiplicative factor (>0) to expand or compress the overall area that the nodes take up
-            concentric: function( node ){ // returns numeric value for each node, placing higher nodes in levels towards the centre
-            return node.degree();
-            },
-            levelWidth: function( nodes ){ // the variation of concentric values in each level
-            return nodes.maxDegree() / 4;
-            },
-            animate: false, // whether to transition the node positions
-            animationDuration: 500, // duration of animation in ms if enabled
-            animationEasing: undefined, // easing of animation if enabled
-            animateFilter: function ( node, i ){ return true; }, // a function that determines whether the node should be animated.  All nodes animated by default on animate enabled.  Non-animated nodes are positioned immediately when the layout starts
-            ready: undefined, // callback on layoutready
-            stop: undefined, // callback on layoutstop
-            transform: function (node, position ){ return position; } // transform a given node position. Useful for changing flow direction in discrete layouts
-            */
         },
           // so we can see the ids 
           // function(node){ console.log( node.data("iconURL")); return `${node.data("iconURL")}`}
@@ -131,7 +103,7 @@ function initGraph(nodes,edges){
                 'label': 'data(name)',
                 'background-color': '#50b46e',
                 'background-image': 'data(iconURL)',
-                'background-image-opacity': 0.5,
+                'background-image-opacity': 1,
                 'background-opacity': 0,
                 'background-fit': 'contain',
                 'background-clip': 'node'
