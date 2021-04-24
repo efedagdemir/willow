a3715971bcb7fdb4906ccfd59b5dc67a33609502
Can Cebeci <can.cebeci99@gmail.com>
2021-04-24 17:36:14 +0300

    minor bugfix

--- demo/GraphOverlay/SidePanel/SidePanel.js
+++ demo/GraphOverlay/SidePanel/SidePanel.js
@@ -98,7 +98,7 @@ document.getElementById("undockBtn").onclick    = () => undockSidePanel(null, tr
 document.getElementById("dockBtn").onclick      = () => dockSidePanel(true);
 document.getElementById("resetBtn").onclick     = () => {chrome.runtime.sendMessage({message: "WILLOW_BACKGROUND_CLEAR_SESSION"})};
 document.getElementById("layoutBtn").onclick    = () => runLayoutAdjustBtn_handler(); // defined in SettingsMenu.js /*{chrome.runtime.sendMessage({message: "WILLOW_GRAPH_VIEWPORT_CENTER"})};*/
-document.getElementById("settingsBtn").onclick  = () => openSettingsMenu();  // defined in SettingsMenu.js
+document.getElementById("settingsBtn").onclick  = () => openSettingsMenu(true);  // defined in SettingsMenu.js
 enableResizing(rightBorderOnly = true);
 
 // -- end of script
