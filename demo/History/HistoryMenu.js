879a4ebd734977ad8daa744392b13f776aa9f0cb
Can Cebeci <can.cebeci99@gmail.com>
2021-04-25 18:25:25 +0300

    v2 complete with session previews

--- demo/History/HistoryMenu.js
+++ demo/History/HistoryMenu.js
@@ -1,4 +1,8 @@
+let previewPane = document.getElementById("previewPane");
+let previewImg = document.getElementById("previewImg");
 let sessionList = document.getElementById("sessionList");
+let curTitle = document.getElementById("curTitle");
+let curSession;
 
 initialize();
 
@@ -8,6 +12,12 @@ function initialize() {
         res.sessions.forEach( session => {
             addSessionElements(session);
         });
+        if (res.sessions.length > 0) {
+            curSession = res.sessions[0];
+            updatePreview();
+        } else {
+            clearPreview();
+        }
     });
 }
 
@@ -17,97 +27,78 @@ function initialize() {
  */
 function addSessionElements (session) {
     console.log(session);
-    // add the html elements
+    // --- create the html elements ---
     let rootDiv = document.createElement("div");
     let titleDiv = document.createElement("div");
     let restoreBtnDiv = document.createElement("div");
     let restoreBtn = document.createElement("button");
     let renameBtnDiv = document.createElement("div");
     let renameBtn = document.createElement("button");
+    let deleteBtnDiv = document.createElement("div");
+    let deleteBtn = document.createElement("button");
         //let image = document.createElement("img");
 
-    // set the properties
+    // --- set the properties ---
     rootDiv.className = "historyElement";
     titleDiv.className = "label";
     restoreBtnDiv.className = "opt";
     renameBtnDiv.className = "opt";
+    deleteBtnDiv.className = "opt";
 
     restoreBtn.title = "Restore this session";
     restoreBtn.innerHTML = "Restore";
     renameBtn.title = "Rename this session";
     renameBtn.innerHTML = "Rename";
+    deleteBtn.title = "Delete this session from your history";
+    deleteBtn.innerHTML = "Delete";
 
-    //image.setAttribute("src", "https://icons.iconarchive.com/icons/paomedia/small-n-flat/256/sign-right-icon.png");
-    if (session.data.name)
-        titleDiv.innerHTML = "<b>" + session.data.name + "</b>";
-    else
-        titleDiv.innerHTML = session.data.lastUpdated;
+    titleDiv.innerHTML = "<b>" + getSessionTitle(session) + "</b>";
     
     restoreBtnDiv.appendChild(restoreBtn);
     renameBtnDiv.appendChild(renameBtn);
+    deleteBtnDiv.appendChild(deleteBtn);
     rootDiv.appendChild(restoreBtnDiv);
     rootDiv.appendChild(renameBtnDiv);
+    rootDiv.appendChild(deleteBtnDiv);
     rootDiv.appendChild(titleDiv);
     //sessionDiv.appendChild(image);
     sessionList.appendChild(rootDiv);
     
+    // --- add listeners ---
+    rootDiv.addEventListener("mouseover", function() {
+        curSession = session;
+        updatePreview();
+    });
     restoreBtn.addEventListener("click", function () {
-        alert("HELLO1");
         chrome.runtime.sendMessage({ message: "WILLOW_HISTORY_LOAD_SESSION", id: session.data.id});
     });
-
     renameBtn.addEventListener("click", function() {
-        alert("HELLO2");
         let name = prompt("Enter new name:");
         chrome.runtime.sendMessage( {message: "WILLOW_HISTORY_RENAME_SESSION", id: session.data.id, name: name});
-        titleDiv.innerHTML = name;
+        titleDiv.innerHTML = "<b>" + name + "</b>";
+        session.data.name = name;    // javascript pls remember this
+        curSession = session;
+        updatePreview();
+    });
+    deleteBtn.addEventListener("click", function () {
+        // TODO
+        alert("GETCHO ASS UP CEM!");
     });
 }
 
-//--------------------------------------------------------------------------------
-/*
-let sessionList = document.getElementById("sessionList");
-
-initialize();
-
-function initialize() {
-    // add session elements for all saved sessions.
-    chrome.storage.local.get( {sessions:[]}, function(res) {
-        res.sessions.forEach( session => {
-            addSessionElements(session);
-        });
-    });
-} */
-
-/**
- * 
- * @param {Object} session A cytoscape export.
- */
-/*
-function addSessionElements (session) {
-    console.log(session);
-    // add the html elements
-    let sessionDiv = document.createElement("div");
-    let title = document.createElement("h1");
-    let image = document.createElement("img");
-    let loadButton = document.createElement("button");
-    let renameButton = document.createElement("button");
-
-    sessionDiv.appendChild(title);
-    sessionDiv.appendChild(image);
-    sessionDiv.appendChild(loadButton);
-    sessionDiv.appendChild(renameButton);
-    sessionList.appendChild(sessionDiv);
-
-    // apply the styles (possibly)
-        // not yet
-
-    // set the properties
-    loadButton.innerHTML = "Load";
-    renameButton.innerHTML = "Rename";
-    image.setAttribute("src", session.data.png);
-    
-
-    
+function updatePreview() {
+    previewPane.style.display = ""; // default in case previously none'd
+    previewImg.setAttribute("src", curSession.data.png);
+    curTitle.innerHTML = getSessionTitle(curSession);
 }
-*/
+
+function clearPreview() {
+    previewPane.style.display = "none";
+}
+
+function getSessionTitle(session) {
+    if (session.data.name)
+        return session.data.name;
+    else
+        return session.data.lastUpdated;
+}
