4f8cc32e12705ccddd7ed669a02eef4adf6c714a
Sezin <sezinzeydan51@gmail.com>
2021-05-02 23:33:25 +0300

    edge lengths shortened

--- demo/SessionGraph.js
+++ demo/SessionGraph.js
@@ -321,7 +321,7 @@ function runLayout(){
         randomize: false,
         nodeDimensionsIncludeLabels: true,
         packComponents: true,
-        /*spacingFactor: 0.69,*/
+        spacingFactor: 0.69,
        
         //contraints
         fixedNodeConstraint: undefined, //fixedCon,
@@ -333,6 +333,7 @@ function runLayout(){
                 return 40;
             else return 200;
         },
+        //idealEdgeLength:20,
 
         ready: () => {},
         stop: () => {}                 
