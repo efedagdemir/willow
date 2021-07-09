2c3937affe2065b7ac77f305e9fc0507d856828d
Selbi Ereshova <selbi.ereshova@ug.bilkent.edu.tr>
2021-07-09 15:55:04 +0300

    Crawler created
    #4 Crawler created

--- demo/Crawler.js
+++ demo/Crawler.js
@@ -1,16 +1,72 @@
+
+
+const cheerio = require('cheerio');
+const fetch = require('node-fetch');
+const urlParser = require('url');
+let mainURL;
+const seenUrls = {};
+
+const getUrl = (link, host, protocol) => {
+    if (link.includes("https")) {
+        return link;
+    } else if (link.startsWith("/")) {
+        return `${protocol}//${host}${link}`;
+    } else {
+        return `${protocol}//${host}/${link}`;
+    }
+};
+async function crawlHelper(URL)
+{
+    const { host, protocol } = await urlParser.parse(URL);
+    console.log("host", host);
+    mainURL =  URL;
+  await  crawl({
+        url: URL,
+        ignore: "/search",
+        host: host,
+        protocol: protocol,
+    });
+console.log( "all done!");
+
+}
+const crawl = async ({ url, ignore, host, protocol }) => {
+    if (seenUrls[url]) return;
+    console.log("crawling", url);
+    seenUrls[url] = true;
+
+    // console.log("host", host);
+    //console.log("mainURL", mainURL);
+
+
+    let urlWithoutPath = protocol + "//" + host;
+    const response = await fetch(url);
+    const html = await response.text();
+    const $ = cheerio.load(html);
+    const links = $("a")
+        .map((i, link) => link.attribs.href)
+        .get();
+
+    // console.log("links",links);
+    links
+        .filter((link) => (link.startsWith(mainURL) || getUrl(link, host, protocol).startsWith(mainURL)) )
+        .forEach((link) => {
+            //  console.log( "taken", getUrl(link, host, protocol));
+            crawl({
+                url: getUrl(link, host, protocol),
+                ignore: ignore,
+                host: host,
+                protocol: protocol,
+            });
+        });
+    // console.log("blank space", "    ");
+};
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (   request.message === "WILLOW_CRAWL")
         {
-            alert(request.URL);
-            crawlURL(request.URL);
+            crawlHelper(request.URL);
         }
     }
 );
 
-
-function crawlURL(URL)
-{
-    const Crawler = require('node-html-crawler');
-    
-}
